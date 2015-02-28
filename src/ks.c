42c8c61240c4d0190ce3956b61cdfbf6bfb6da5f
Athif Wulandana <athif@u.northwestern.edu>
2015-02-27 22:34:18 -0600

    Hello world!

---
+++ src/ks.c
@@ -0,0 +1,36 @@
+#include <pebble.h>
+  
+static Window*window;
+static TextLayer* text_layer;
+
+static void window_load(Window* window){
+  Layer* window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  text_layer=text_layer_create((GRect) {
+    .origin={0,62},
+    .size = {bounds.size.w,30}
+  });
+  text_layer_set_text(text_layer, "Hello world!");
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+static void window_unload(Window* window){
+  text_layer_destroy(text_layer);
+}
+static void init(void){
+  window=window_create();
+  window_set_window_handlers(window, (WindowHandlers){
+    .load = window_load,
+    .unload = window_unload
+  });
+  bool animated=true;
+  window_stack_push(window, animated);
+}
+static void deinit(void){
+  window_destroy(window);
+}
+  
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
